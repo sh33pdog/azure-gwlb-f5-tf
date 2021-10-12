e8a0795d7f83b685c843558d11c02f85d31d2237
mikeoleary <michael.w.oleary@gmail.com>
2021-10-12 13:57:19 -0400

    updates for standardization - demo web app is now f5devcentral/f5-demo-httpd:nginx - outputs updated to be clearer for user verification - Updates to Readme for better instructions

--- outputs.tf
+++ outputs.tf
@@ -1,10 +1,15 @@
-output "application_url" {
+output "web_address_for_application" {
   description = "Public URL to access web app"
   value       = "http://${azurerm_public_ip.public_lb_frontend_ip.ip_address}/"
 }
-output "ssh_address" {
+output "ssh_address_for_app_server" {
   description = "SSH to app server"
   value       = "ssh azureuser@${azurerm_public_ip.public_lb_frontend_ip.ip_address}"
+}
+
+output "bigip_mgmt_public_ip_addresses" {
+  description = "public ip for bigip mgmt console"
+  value       = module.bigip.*.mgmtPublicIP
 }
 output "bigip_mgmt_username" {
   description = "username for bigip mgmt console"
